8f7513e0457579b0a920e648dd2ef2e1f22c0abb
OlgaShreyn <shrein@skbkontur.ru>
2021-11-26 01:03:24 +0500

    first part of test

--- SelenClassLibrary1/SelenClassLibrary1/Homework1.cs
+++ SelenClassLibrary1/SelenClassLibrary1/Homework1.cs
@@ -4,11 +4,13 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Interactions;
 using OpenQA.Selenium.Support.UI;
+
 namespace SelenClassLibrary1
 {
     public class Homework1
     {
         private IWebDriver driver;
+        private WebDriverWait wait;
 
         [SetUp]
         public void SetUp()
@@ -18,6 +20,8 @@ namespace SelenClassLibrary1
             options.AddArgument("incognito");
             driver = new ChromeDriver(options);
             driver.Navigate().GoToUrl("https://www.labirint.ru/");
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
         }
 
         [Test]
@@ -30,18 +34,49 @@ namespace SelenClassLibrary1
             var issueOrder = By.XPath("(//a[contains(@id, 'buy') and text()='ОФОРМИТЬ'])[1]");
             var beginOrder = By.Id("basket-default-begin-order");
             var chooseCourierDelivery = By.CssSelector("[data-delivery-type-name=courier] .b-radio-e-bg");
-            
+
             var city = By.CssSelector(".b-form-input[data-suggeststype=district]");
             var street = By.CssSelector(".b-form-input[data-suggeststype=streets]");
-            var building  = By.CssSelector(".b-form-input[name^=building]");
-            var flat  = By.CssSelector(".b-form-input[name^=flat]");
+            var building = By.CssSelector(".b-form-input[name^=building]");
+            var flat = By.CssSelector(".b-form-input[name^=flat]");
             var cityError = By.CssSelector(".b-form-e-row-m-district [id^=formvalidate-label");
-            
-            var suggestedCity  = By.CssSelector("a[id^=suggest]");
-            var confirm  = By.CssSelector(".responsive-children .js-dlform-wrap [value=Готово]");
-            var courierDeliveryLightbox  = By.CssSelector(".responsive-children .js-dlform-wrap");
+
+            var suggestedCity = By.CssSelector("a[id^=suggest]");
+            var confirm = By.CssSelector(".responsive-children .js-dlform-wrap [value=Готово]");
+            var courierDeliveryLightbox = By.CssSelector(".responsive-children .js-dlform-wrap");
+            var loader = By.ClassName("loading-panel-inner");
+            var d = By.Id("dp1637843263067");
+
+            driver.FindElement(cookiePolicyAgree).Click();
+            new Actions(driver)
+                .MoveToElement(driver.FindElement(booksMenu))
+                .Click(driver.FindElement(allBooks))
+                .Build()
+                .Perform();
+            Assert.That(driver.Url == "https://www.labirint.ru/books/");
+            driver.FindElement(addBookInCart).Click();
+            driver.FindElement(issueOrder).Click();
+            driver.FindElement(beginOrder).Click();
+            driver.FindElement(chooseCourierDelivery).Click();
+            var cityElement = driver.FindElement(city);
+            cityElement.SendKeys("lala");
+            cityElement.SendKeys("\t");
+            Assert.That(driver.FindElement(cityError).Displayed);
+            cityElement.Clear();
+            cityElement.SendKeys("Екатеринбург");
+            driver.FindElement(suggestedCity).Click();
+            driver.FindElement(street).SendKeys("Малопрудная ул.");
+            driver.FindElement(building).SendKeys("5");
+            driver.FindElement(flat).SendKeys("600");
+
+            //wait.Until(ExpectedConditions.ElementIsVisible(driver.FindElement(loader)));
+            (driver as IJavaScriptExecutor)?.ExecuteScript(
+                $"$('[id='dp1637843263067']).datepicker('setDate','{DateTime.Today.AddDays(8).ToString("dd.MM.yyyy")}')");
+            //wait.Until(ExpectedConditions.ElementIsVisible(driver.FindElement(loader)));
+            driver.FindElement(confirm).Click();
+            Assert.That(driver.FindElement(courierDeliveryLightbox).Displayed == false);
         }
-        
+
         [TearDown]
         public void TearDown()
         {
